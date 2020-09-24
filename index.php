5114f33ee7119f1cc52cee11e9fcd6d0d008e423
jonathan <jonathanentila10@gmail.com>
2020-09-24 12:28:49 +0800

    name

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// @oxygendigital - jonathan entila
+/*
+ * MetaSlider. Slideshow plugin for WordPress.
+ *
+ * Plugin Name: OxygenDigital Logo Slider
+ * Plugin URI:  https://www.oxygendigital.co.nz
+ * Description: Logo slider only nothing special.
+ * Version:     1.0
+ * Author:      OD - Jonathan Entila
+ * Author URI:  https://www.oxygendigital.co.nz
+ * License:     GPL-2.0+
+ * Copyright:   2020- Oxygen Digital
+ *
+ * Text Domain: od-slider
+ */
+
+if (!defined('ABSPATH')) {
+    die('No direct access.');
+}
