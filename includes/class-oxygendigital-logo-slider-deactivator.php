9db8a2d418d51536641ba61b16ce24fe08d67951
jonathan <jonathanentila10@gmail.com>
2020-09-24 12:39:27 +0800

    plugins-boiler

---
+++ includes/class-oxygendigital-logo-slider-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       oxygendigital.co.nz
+ * @since      1.0.0
+ *
+ * @package    Oxygendigital_Logo_Slider
+ * @subpackage Oxygendigital_Logo_Slider/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Oxygendigital_Logo_Slider
+ * @subpackage Oxygendigital_Logo_Slider/includes
+ * @author     Jonathan <jonathan.entila@oxygendigital.co.nz>
+ */
+class Oxygendigital_Logo_Slider_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
