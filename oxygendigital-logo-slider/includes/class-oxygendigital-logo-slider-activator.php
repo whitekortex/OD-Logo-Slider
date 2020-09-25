e25425b8f42e10f194727f084f30cb5aeb007499
jonathan <jonathanentila10@gmail.com>
2020-09-25 15:54:29 +0800

    re-brand 5 slides show

---
+++ oxygendigital-logo-slider/includes/class-oxygendigital-logo-slider-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       oxygendigital.co.nz
+ * @since      1.0.0
+ *
+ * @package    Oxygendigital_Logo_Slider
+ * @subpackage Oxygendigital_Logo_Slider/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Oxygendigital_Logo_Slider
+ * @subpackage Oxygendigital_Logo_Slider/includes
+ * @author     Jonathan <jonathan.entila@oxygendigital.co.nz>
+ */
+class Oxygendigital_Logo_Slider_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
